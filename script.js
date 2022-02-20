3b709e9f05e4c755aaf5636c6da7a99f2bbd3ae9
Cedric Lary <75456812+larylc@users.noreply.github.com>
2022-02-20 16:40:03 -0500

    first personal website launch

---
+++ script.js
@@ -0,0 +1,53 @@
+let theme_change = localStorage.getItem("theme-change")
+
+if(theme_change == null){
+  setTheme("light")
+}else{
+  setTheme(theme_change)
+}
+
+
+
+const waffle = d3.select('.waffle');
+ 
+const numbers = d3.range(260);
+   
+
+waffle
+	.selectAll('.block')
+	.data(numbers)
+	.enter()
+	.append('div')
+	.attr('class', 'block')
+	
+//Themes
+
+
+let themes = document.getElementsByClassName("theme");
+
+for(var i= 0; themes.length > i; i++){
+  themes[i].addEventListener("click", function(){
+    let mode = this.dataset.mode;
+    console.log("Option Clicked: ", mode);
+    setTheme(mode);
+    
+  })
+}
+
+
+function setTheme(mode) {
+  if(mode == "light"){
+    document.getElementById("theme-style").href = "light.css"
+  }
+    if(mode == "salmon"){
+    document.getElementById("theme-style").href = "salmon.css"
+  }
+    if(mode == "sea"){
+    document.getElementById("theme-style").href = "sea.css"
+  }
+    if(mode == "dark"){
+    document.getElementById("theme-style").href = "dark.css"
+  }
+  localStorage.setItem("theme-change", mode)
+  
+}
